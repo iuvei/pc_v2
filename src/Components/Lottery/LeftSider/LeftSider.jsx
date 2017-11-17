2737443d824478def1ae5cc97085ad9c7751b94e
zack hengcai <zack@hengcai88.com>
2017-11-17 19:23:22 +0800

    1117-1

--- src/Components/Lottery/LeftSider/LeftSider.jsx
+++ src/Components/Lottery/LeftSider/LeftSider.jsx
@@ -33,7 +33,7 @@ export default class LeftSider extends Component {
                               mode="vertical"
                               theme="dark"
                         >
-                            <SubMenu key="sub1" title={<span><img src={left_1} alt=""/></span>}>
+                            <SubMenu key="sub1"  title={<span className="l_s_bg_01"></span>}>
                                 <MenuItemGroup title="24小时">
                                     <Menu.Item key="24_1">泰国秒秒彩</Menu.Item>
                                     <Menu.Item key="24_2">泰国300秒</Menu.Item>
@@ -42,7 +42,7 @@ export default class LeftSider extends Component {
                                     <Menu.Item key="24_5">河内五分彩</Menu.Item>
                                 </MenuItemGroup>
                             </SubMenu>
-                            <SubMenu key="sub2" title={<span><img src={left_2_active} alt=""/></span>}>
+                            <SubMenu key="sub2" title={<span className="l_s_bg_02"></span>}>
                                 <MenuItemGroup title="时时彩系列">
                                     <Menu.Item key="ss_1">重庆时时彩</Menu.Item>
                                     <Menu.Item key="ss_2">新疆时时彩</Menu.Item>
@@ -51,7 +51,7 @@ export default class LeftSider extends Component {
                                     <Menu.Item key="ss_5">北京五分彩</Menu.Item>
                                 </MenuItemGroup>
                             </SubMenu>
-                            <SubMenu key="sub3" title={<span><img src={left_3} alt=""/></span>}>
+                            <SubMenu key="sub3" title={<span className="l_s_bg_03"></span>}>
                                 <MenuItemGroup title="11选5系列">
                                     <Menu.Item key="11_1">江西11选5</Menu.Item>
                                     <Menu.Item key="11_2">重庆11选5</Menu.Item>
@@ -60,7 +60,7 @@ export default class LeftSider extends Component {
                                     <Menu.Item key="11_5">广东11选5</Menu.Item>
                                 </MenuItemGroup>
                             </SubMenu>
-                            <SubMenu key="sub4" title={<span><img src={left_4} alt=""/></span>}>
+                            <SubMenu key="sub4" title={<span className="l_s_bg_04"></span>}>
                                 <MenuItemGroup title="快三系列">
                                     <Menu.Item key="ks_1">江苏快三</Menu.Item>
                                     <Menu.Item key="ks_2">北京快三</Menu.Item>
@@ -68,7 +68,7 @@ export default class LeftSider extends Component {
                                     <Menu.Item key="ks_4">泰国快三</Menu.Item>
                                 </MenuItemGroup>
                             </SubMenu>
-                            <SubMenu key="sub5" title={<span><img src={left_5} alt=""/></span>}>
+                            <SubMenu key="sub5" title={<span className="l_s_bg_05"></span>}>
                                 <MenuItemGroup title="其他系列">
                                     <Menu.Item key="qt_1">排列三</Menu.Item>
                                     <Menu.Item key="qt_2">福彩3D</Menu.Item>
